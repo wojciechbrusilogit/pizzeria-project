5cadb1d98d5fbedc50ab4d0ffedd434c7ce1f334
Wojciech Brusilo <brusilo124@gmail.com>
2023-02-09 20:45:51 +0100

    Koniec modułu 7 :D

--- dist/js/script.js
+++ dist/js/script.js
@@ -94,6 +94,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper)
 
     }
 
@@ -143,36 +144,43 @@
 
     processOrder() {
       const thisProduct = this;
+      // covertform to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData: ', formData);
-
+      console.log('formData', formData);
       // set price to default price
       let price = thisProduct.data.price;
       // for every category (param)...
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
         // for every option in this category
         for (let optionId in param.options) {
-
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-
-          // check if there is a param with a name of paramId and if includes optionId
+          console.log(optionId, option);
+          // check if there is param with a name of paramId in formData and if it includes optionId
           if (formData[paramId] && formData[paramId].includes(optionId)) {
-
-            // chceck if the option is not default 
-            if (!option.default) {
-
+            // check if the option is not default
+            if (!option.default == true) {
               // add option price to price variable
-              price += option.price;
+              price = option.price + price;
             }
           } else {
-            // chceck if the option is default
-            if (option.default) {
-
+            // check if the option is default
+            if (option.default == true) {
               // reduce price variable
-              price -= option.price;
+              price = price - option.price;
+            }
+          }
+          //find images that passt to category-option
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId); //(paramId + optionId) - nie dziala;
+          // check if you found the image
+          if (optionImage) {
+            if (formData[paramId] && formData[paramId].includes(optionId)) {
+              optionImage.classList.add('active'); //add class active
+            } else {
+              optionImage.classList.remove('active'); //remove classactive
             }
           }
         }
@@ -181,6 +189,8 @@
       thisProduct.priceElem.innerHTML = price;
     }
   }
+
+
   const app = {
     initMenu: function () {
       const thisApp = this;
